10cc4902b0a2874cf49e4635009c9c5a0918bc5e
Sergei-Vorfolomeev <vorfo1897@gmail.com>
2022-10-11 23:47:25 +0200

    Add components (Heading, Navbar, Profile)

---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Heading = () => {
+    return (
+        <header className="header">
+            <img src="https://i.pinimg.com/originals/5a/bb/e8/5abbe86ddb609ac264313f3805d9a1cd.jpg"
+                 alt="logo"></img>
+        </header>)
+}
